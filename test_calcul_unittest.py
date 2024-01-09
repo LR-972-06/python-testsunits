47fb2ae999d9ebf2fcacfbaa6905c12fbf7e0542
LR-972-06 <rlauriane@gmail.com>
2024-01-09 12:17:34 +0100

    divZero en comm et correction

--- test_calcul_unittest.py
+++ test_calcul_unittest.py
@@ -35,16 +35,16 @@ class TestCalcul(unittest.TestCase):
         # Vérifier si le résultat est égal à 3
         self.assertEqual(resultat, 3)
 
-    # # # Test de division par zéro
-    # # def test_diviserZero(self):
-    # #     resultat = self.calc.diviser(72, 0)
-    # #     # Vérifier si la division par 0 est gérée
-    # #     self.assertEqual(resultat,"Division par zéro impossible")
+    # # Test de division par zéro
+    # def test_diviserZero(self):
+    #     resultat = self.calc.diviser(72, 0)
+    #     # Vérifier si la division par 0 est gérée
+    #      self.assertEqual(resultat,"Division par zéro impossible")
     
     # Test de division par zéro - exception
     def test_diviser_par_zero(self):
         with self.assertRaises(ZeroDivisionError, msg="Division par zéro impossible"):
-                    self.calc.diviser(5, 0)
+                self.calc.diviser(5, 0)
 
 # Exécuter les tests si le script est exécuté directement
 if __name__ == '__main__':
